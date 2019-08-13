6b61e165f71e7080746c0408250de327c5e6c86e
Matthew Einhorn <moiein2000@gmail.com>
2019-08-12 23:20:02 -0400

    Log gl calls.

--- kivy/core/window/window_pyodide.py
+++ kivy/core/window/window_pyodide.py
@@ -22,15 +22,20 @@ class WindowPyodide(WindowBase):
 
     def __init__(self, **kwargs):
         self._win = self
+        Logger.warn('Before init')
         super().__init__(**kwargs)
+        Logger.warn('After init')
 
     def _get_gl_size(self):
         w, h = self.system_size
+        Logger.warn('system size, {}, {}'.format(w, h))
         return self._density * w, self._density * h
 
     def initialize_gl(self):
+        Logger.warn('Before gl')
         from kivy.graphics.cgl_backend.cgl_pyodide import set_pyodide_gl
         self._pyodide_gl = context = self._pyodide_canvas.getContext("webgl2")
+        Logger.warn('After gl')
 
         backing_store = (
             getattr(context, 'backingStorePixelRatio', 0) or
@@ -45,27 +50,35 @@ class WindowPyodide(WindowBase):
             getattr(js_window, 'devicePixelRatio', 0) or 1) / backing_store
         self.dpi = self._density * 96.
 
+        Logger.warn('Before setting gl')
         set_pyodide_gl(self._pyodide_gl)
+        Logger.warn('After setting gl')
         super().initialize_gl()
 
     def create_window(self):
         if self.initialized:
+            Logger.warn('Before reinit')
             w, h = self.system_size
             self._pyodide_canvas.setAttribute('width', w)
             self._pyodide_canvas.setAttribute('height', h)
+            Logger.warn('After reinit')
             super(WindowPyodide, self).create_window()
             return
 
+        Logger.warn('Before canvas')
         self._pyodide_canvas = canvas = iodide.output.element('canvas')
+        Logger.warn('After canvas')
 
         def ignore(event):
             event.preventDefault()
             return False
         js_window.addEventListener('contextmenu', ignore)
 
+        Logger.warn('Setting size')
         w, h = self.system_size
         canvas.setAttribute('width', w)
         canvas.setAttribute('height', h)
+        Logger.warn('Set size')
 
         # self._focus = True
 
@@ -76,12 +89,14 @@ class WindowPyodide(WindowBase):
 
     def close(self):
         # self._win.teardown_window()
+        Logger.warn('Before close')
         from kivy.graphics.cgl_backend.cgl_pyodide import clear_pyodide_gl
         super().close()
         self._pyodide_canvas = None
         self._pyodide_gl = None
         self.initialized = False
         clear_pyodide_gl()
+        Logger.warn('After close')
 
     def flip(self):
         pass
